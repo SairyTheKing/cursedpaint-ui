4de478fa7308e825a33d29b6ce539a82d76858fc
SairyTheKing <SairyTheKing@users.noreply.github.com>
2026-06-10 18:37:01 +0700

    Cache bust loader source request

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/SairyTheKing/cursedpaint-ui/main/Source.lua"))()
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/SairyTheKing/cursedpaint-ui/main/Source.lua?v=0.6.2"))()
